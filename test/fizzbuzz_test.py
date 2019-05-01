884ee205f2fef045dd12b301c5404f776159853e
Jimjule <roadknightj@gmail.com>
2019-05-01 12:58:02 +0100

    Refactored normal number test to include edge cases

--- test/fizzbuzz_test.py
+++ test/fizzbuzz_test.py
@@ -6,8 +6,15 @@ sys.path.insert(0,'/Users/James/Projects/Python_Playground/fizzbuzz_python/lib')
 
 from fizzbuzz import Fizzbuzz as fb
 
-def test_one():
-    assert fb.go(1) == 1
+def test_normal_numbers():
+    count = 0
+    if fb.go(1) == 1:
+        count += 1
+    if fb.go(100000003) == 100000003:
+        count += 1
+    if fb.go(8) == 8:
+        count += 1
+    assert count == 3
 
 def test_multiple_of_three_is_fizz():
     count = 0
